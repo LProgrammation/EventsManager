73b9bc9bbcd3469a910cdbdd36d1c54fa8fdb99c
Louis Potdevin <l.potdevin.informatique@gmail.com>
2025-11-19 12:00:28 +0100

    Test

--- public/index.php
+++ public/index.php
@@ -10,14 +10,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-try {
-    if ($_SERVER['REQUEST_URI'] !== '/errors/1045') {
-        $db = Database::getInstance();
-        $pdo = $db->getConnection();
-    }
-    Router::dispatch();
-} catch (PDOException $e) {
-        header('Location: /errors/1045');
-        exit;
+phpinfo();
+// try {
+//     if ($_SERVER['REQUEST_URI'] !== '/errors/1045') {
+//         $db = Database::getInstance();
+//         $pdo = $db->getConnection();
+//     }
+//     Router::dispatch();
+// } catch (PDOException $e) {
+//         header('Location: /errors/1045');
+//         exit;
     
-}
+// }
