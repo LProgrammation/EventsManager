bff5f885fc459196037e182935a8d4e4a7f512a3
Louis Potdevin <l.potdevin.informatique@gmail.com>
2025-11-19 12:01:58 +0100

    Test Canceled

--- public/index.php
+++ public/index.php
@@ -10,15 +10,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-phpinfo();
-// try {
-//     if ($_SERVER['REQUEST_URI'] !== '/errors/1045') {
-//         $db = Database::getInstance();
-//         $pdo = $db->getConnection();
-//     }
-//     Router::dispatch();
-// } catch (PDOException $e) {
-//         header('Location: /errors/1045');
-//         exit;
+try {
+    if ($_SERVER['REQUEST_URI'] !== '/errors/1045') {
+        $db = Database::getInstance();
+        $pdo = $db->getConnection();
+    }
+    Router::dispatch();
+} catch (PDOException $e) {
+        header('Location: /errors/1045');
+        exit;
     
-// }
+}
